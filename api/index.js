297ac3a5321ba1ced1599e3fd9a899d84ab185d9
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-20 21:08:42 +0300

    updates changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,12 @@ const dotenv = require("dotenv");
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer = require("multer")
+const path = require("path");
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
 
 dotenv.config();
 app.use(express.json());
@@ -15,8 +21,25 @@ mongoose.connect(process.env.MONGO_URL,{
 .then(console.log("Connected to Mongodb"))
 .catch(err => console.log(err));
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, 'images'); 
+    },
+    filename: (req,file,cb) =>{
+        cb(null,req.body.name)
+    },
+})
+
+const upload = multer({storage: storage});
+app.post("api/upload",upload.single("file"),(req,res) => {
+    res.status(200).json("File has been uploaded");
+})
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
 
 // Importing routes
 app.listen("5000", ()=>{
